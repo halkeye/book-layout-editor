a644dbcbd7a2818efa81a2f9027fc2ffadf0128c
Gavin <gavin@kodekoan.com>
2013-02-08 16:33:27 -0800

    initial bootstrap from yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var bookLayoutEditorApp = angular.module('bookLayoutEditorApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
