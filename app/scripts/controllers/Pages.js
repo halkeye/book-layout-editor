bfe3690e1aa60d8d9dbc8efc77b2e146fc98f5c3
Gavin <gavin@kodekoan.com>
2013-02-08 19:04:57 -0800

    page views and fluid layout

--- app/scripts/controllers/Pages.js
+++ app/scripts/controllers/Pages.js
@@ -3,7 +3,12 @@
 (function () {
   'use strict';
 
-  window.bookLayoutEditorApp.controller('PagesCtrl', function($scope) {
+  function PagesCtrl($scope, $params) {
+    $scope.pageNumber = $params.pageNumber;
     $scope.pages = [1,2,3,4,5];
-  });
+  }
+
+  PagesCtrl.$inject = ['$scope','$routeParams'];
+
+  window.bookLayoutEditorApp.controller('PagesCtrl',PagesCtrl);
 })();
