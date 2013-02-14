78b69bfd0df1478dbf4b3fff626a5f242460ca01
Gavin <gavin@kodekoan.com>
2013-02-14 01:41:00 -0800

    updating gh-pages

---
+++ site/scripts/controllers/db4ab41a.NavController.js
@@ -0,0 +1,37 @@
+/*global angular:false*/
+/*jshint browser:true devel:true */
+(function () {
+  'use strict';
+  window.bookLayoutEditorApp.controller('NavController', ['$scope', '$http', '$location', '$rootScope', '$route', function($scope, $http, $location, $rootScope, $route) {
+    // Default Value
+    $scope.book_language = 'en';
+    // All Pages
+    $scope.book_pages = $rootScope.book_data.PAGES;
+    // All Languages
+    var langs =  Object.keys($rootScope.book_data.PAGES);
+    if (!langs.length) {
+      $scope.book_page_languages = [{ val: 'en', name: window.getLanguageName('en')}];
+    } else {
+      $scope.book_page_languages = [];
+      angular.forEach(langs, function(val) {
+        $scope.book_page_languages.push({ val: val, name: window.getLanguageName(val)});
+      });
+    }
+
+    $scope.navCollection = [
+      {id:'home', title:'Home'}
+    ];
+
+    $scope.navClass = function (page) {
+      var currentRoute = $location.path().substring(1) || 'home';
+      return page === currentRoute ? 'active' : '';
+    };
+
+    $scope.navigateToSection = function (sectionID) {
+      $location.path('/'+sectionID);
+    };
+    $scope.navigateToPage = function (lang,pageNum) {
+      $location.path('/pages/'+lang+'/'+pageNum);
+    };
+  }]);
+})();
